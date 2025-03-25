52873dd49f51cbd4ad0854164820104f1b1409a9
Nadia Sabivchak <sabivcaknada@gmail.com>
2025-03-25 19:13:55 +0200

    Added calculateRentalCost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const costPerDay = 40;
+  let totalCost = days * costPerDay;
+
+  if (days >= 7) {
+    totalCost -= 50; // Знижка $50 при оренді 7+ днів
+  } else if (days >= 3) {
+    totalCost -= 20; // Знижка $20 при оренді 3+ днів
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
